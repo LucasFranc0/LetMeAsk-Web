97a6fc21ab6df79bfcad15a692aa518808c6f40f
Franco <lucasfranconx@gmail.com>
2023-10-27 13:25:30 -0400

    Config Changes

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,13 +6,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCblmPQzel7r4u70ZxdCrFmDmeGXBK3J64",
-  authDomain: "letmeask-26ec4.firebaseapp.com",
-  databaseURL: "https://letmeask-26ec4-default-rtdb.firebaseio.com",
-  projectId: "letmeask-26ec4",
-  storageBucket: "letmeask-26ec4.appspot.com",
-  messagingSenderId: "686340601527",
-  appId: "1:686340601527:web:7822b3ca4bdd1ace373f32",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
